9114dcc6adae8da4a5fecc1fbbf68e21373128f4
vivek-gohil <59043694+vivek-gohil@users.noreply.github.com>
2022-07-30 11:18:53 +0530

    Add files via upload

---
+++ CUSTOMER-QUERY.sql
@@ -0,0 +1,13 @@
+CREATE TABLE customer_details(
+    id NUMBER,
+    first_name VARCHAR2(50),
+    last_name VARCHAR2(50),
+    email VARCHAR2(50),
+    gender VARCHAR2(10),
+    contact_no VARCHAR2(20),
+    country VARCHAR2(20),
+    dob DATE
+);
+
+COMMIT;
+
